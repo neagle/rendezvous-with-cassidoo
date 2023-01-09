d4afce4bf7805ad3ade94d16e6c5edc0a7a54b74
Nate Eagle <nate.eagle@greymatter.io>
2023-01-09 11:37:20 -0500

    add minor change

--- 282/sum-every-other.js
+++ 282/sum-every-other.js
@@ -6,12 +6,7 @@ const sumEveryOther = (num) => {
 		// Toss out anything that's not a number
 		.filter((char) => /[0-9]/.test(char))
 		// Sum up every other number (all the odds)
-		.reduce((acc, curr, i) => {
-			if (i % 2 === 1) {
-				return acc + Number(curr)
-			}
-			return acc
-		}, 0)
+		.reduce((acc, curr, i) => (i % 2 === 1 ? acc + Number(curr) : acc), 0)
 
 	return sum
 }
--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 * Add script to automatically create a new folder / files for new challenge,
   using cli to ask for script name and challenge text
+	- have it generate image with https://carbon.now.sh/
+	- have it automatically generate tweet reply to cassidoo
 
 * Set up Github action to run npm test for new PRs
